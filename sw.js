234e7e96f86f2734f73a5ca63cb808c9cf8d2036
ikhalihnnatamlougou-maker <ikhalihnna.tamlougou@gmail.com>
2026-06-27 00:41:49 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+// تغيير الرقم هنا إلى v2 يجبر المتصفح على التحديث
+const cacheName = 'IHSAN-v2'; 
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+  // تفعيل النسخة الجديدة فوراً
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== cacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
