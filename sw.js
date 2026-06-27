0972c942057b1c8acbb3c59236c1090170951023
ikhalihnnatamlougou-maker <ikhalihnna.tamlougou@gmail.com>
2026-06-27 03:55:51 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,37 @@
-self.addEventListener('fetch', function(event) {});
+// تغيير الرقم هنا إلى v2 يجبر المتصفح على التحديث
+const cacheName = 'Ihsan-v3'; 
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+  // تفعيل النسخة الجديدة فوراً
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== cacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
